19ebb21e6245917fd7dc0d0465a5a7d95bdb5007
Diego <Diego@MacBook-Air-de-Diego.local>
2019-07-03 00:48:42 -0500

    se agrega swit alert

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import { Usuario } from '../../models/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICES } from '../../config/config';
+import { Observable } from 'rxjs';
+
+import { map, catchError, timeout, switchAll } from 'rxjs/operators';
+import * as swal from 'sweetalert';
+
+import 'rxjs/add/operator/map'
 
 
 @Injectable({
@@ -16,8 +22,11 @@ export class UsuarioService {
   crearUsuario( usuario: Usuario){
     let url = URL_SERVICES + '/usuario';
 
-    return this.http.post(url, usuario);
-
+    return this.http.post(url, usuario)
+    .pipe(map( (resp:any)=>{
+        swal('Usuario creado', usuario.nombre, 'success');
+        return resp.usuario;
+      }));
 
   }
 }
